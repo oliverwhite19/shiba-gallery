8b87b609f5f5e04fe44eac642f9f2df81d26a81e
Oliver White <whiteoliverg@gmail.com>
2022-07-24 10:34:52 -0400

    some styling and formatting

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import useFetch from 'react-fetch-hook';
 import useTrigger from 'react-use-trigger/useTrigger';
 import createTrigger from 'react-use-trigger';
 import { Image } from './components';
+import { CircularProgress, Grid, IconButton } from '@mui/material';
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 const requestTrigger = createTrigger();
+
 function App() {
     const requestTriggerValue = useTrigger(requestTrigger);
     const [index, setIndex] = useState(0);
@@ -17,31 +21,36 @@ function App() {
 
     return (
         <div>
-            <header>
-                <p>Let's go get some doges for you all</p>
-            </header>
             {isLoading ? (
-                <p>Loading images...</p>
+                <CircularProgress color="success" />
             ) : (
-                <>
-                    <button
-                        onClick={() => setIndex(index - 1)}
-                        disabled={!index}
-                    >
-                        Previous
-                    </button>
-                    <Image src={data?.[index]} alt="a shiba pic" />
-                    <button
-                        onClick={() => {
-                            if (index === shibas.length - 1) {
-                                requestTrigger();
-                            }
-                            setIndex(index + 1);
-                        }}
-                    >
-                        Next
-                    </button>
-                </>
+                <Grid container spacing={2}>
+                    <Grid item xs>
+                        <IconButton
+                            onClick={() => setIndex(index - 1)}
+                            disabled={!index}
+                            arial-label="previous image"
+                        >
+                            <ArrowLeftIcon />
+                        </IconButton>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Image src={data?.[index]} alt="a shiba pic" />
+                    </Grid>
+                    <Grid item xs>
+                        <IconButton
+                            onClick={() => {
+                                if (index === shibas.length - 1) {
+                                    requestTrigger();
+                                }
+                                setIndex(index + 1);
+                            }}
+                            arial-label="next image"
+                        >
+                            <ArrowRightIcon />
+                        </IconButton>
+                    </Grid>
+                </Grid>
             )}
         </div>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
