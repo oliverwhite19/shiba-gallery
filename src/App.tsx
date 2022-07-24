021fb6885f650a1ceb24bc3572be438b56487774
Oliver White <whiteoliverg@gmail.com>
2022-07-24 10:14:56 -0400

    basic loading of the doges

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import useFetch from 'react-fetch-hook';
+import useTrigger from 'react-use-trigger/useTrigger';
+import createTrigger from 'react-use-trigger';
+import { Image } from './components';
+const requestTrigger = createTrigger();
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const requestTriggerValue = useTrigger(requestTrigger);
+    const [index, setIndex] = useState(0);
+    const [shibas, setShibas] = useState<Array<string>>([]);
+    const { isLoading, data } = useFetch<Array<string>>(
+        'http://shibe.online/api/shibes?count=10',
+        {
+            depends: [requestTriggerValue],
+        }
+    );
+
+    return (
+        <div>
+            <header>
+                <p>Let's go get some doges for you all</p>
+            </header>
+            {isLoading ? (
+                <p>Loading images...</p>
+            ) : (
+                <>
+                    <button
+                        onClick={() => setIndex(index - 1)}
+                        disabled={!index}
+                    >
+                        Previous
+                    </button>
+                    <Image src={data?.[index]} alt="a shiba pic" />
+                    <button
+                        onClick={() => {
+                            if (index === shibas.length - 1) {
+                                requestTrigger();
+                            }
+                            setIndex(index + 1);
+                        }}
+                    >
+                        Next
+                    </button>
+                </>
+            )}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Image.tsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+const Image = styled.img`
+    max-width: 80vw;
+    max-height: 80vh;
+`;
+
+export { Image };
